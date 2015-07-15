85112688e3c4eda1c95874625da0e63da36c5370
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-07-15 04:57:51 +0000

    minor tweaks to courses controller test

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -5,10 +5,10 @@ class CoursesControllerTest < ActionController::TestCase
    test "course created" do 
    	 user = FactoryGirl.create(:user)
    	 sign_in user
-   	 course = FactoryGirl.create(:course)
+   	 course = FactoryGirl.create(:course, :user => user)
 
    	 assert_difference 'Course.count' do
-   	 	post :create, :course_id => course.id, :course => {title: course.title, description: course.description, cost: course.cost}
+   	 	post :create, :course => {title: course.title, description: course.description, cost: course.cost}
    	 end
    	 # assert_equal 1, courses.count
    	 # assert_redirected_to instructor_course_path(course)
