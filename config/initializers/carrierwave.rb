a0070d4a168093b717435794e87f07451af466cd
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-07-19 22:03:38 +0000

    slight tweaks to lesson styling

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,13 +2,13 @@
 
 CarrierWave.configure do |config|
   config.storage    = :aws
-  config.aws_bucket = ENV.fetch('AWS_BUCKET')
+  config.aws_bucket = ENV['AWS_BUCKET']
   config.aws_acl    = 'public-read'
 
 
   config.aws_credentials = {
-    access_key_id:     ENV.fetch('AWS_ACCESS_KEY'),
-    secret_access_key: ENV.fetch('AWS_SECRET_KEY'),
-    region:            ENV.fetch('AWS_REGION') # Required
+    access_key_id:     ENV['AWS_ACCESS_KEY'],
+    secret_access_key: ENV['AWS_SECRET_KEY'],
+    region:            ENV['AWS_REGION'] # Required
   }
 end
