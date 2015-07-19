4f465b3affc2208fae9e7cae4b47c2b80cc625d4
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-07-19 14:40:04 +0000

    added video uploading via carrierwave-aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,14 @@
 # config/initializers/carrierwave.rb
- 
+
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
+  config.storage    = :aws
+  config.aws_bucket = ENV.fetch('AWS_BUCKET')
+  config.aws_acl    = 'public-read'
+
+
+  config.aws_credentials = {
+    access_key_id:     ENV.fetch('AWS_ACCESS_KEY'),
+    secret_access_key: ENV.fetch('AWS_SECRET_KEY'),
+    region:            ENV.fetch('AWS_REGION') # Required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]                     # required
 end
