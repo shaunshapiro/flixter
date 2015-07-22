02896ad119fc94cd92b1a688bd24bae13bf203fe
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-07-22 00:53:29 +0000

    added authentication so only enrolled users can access lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,26 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_course
+
 	def show
 	end
 
 	private
+
 	helper_method :current_lesson
+
+
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
+	end
+
+
+
+	
+
+	def require_authorized_for_current_course
+		if ! current_user.enrolled_in?(current_lesson.section.course) 
+			redirect_to course_path(current_lesson.section.course), :alert => 'You must be enrolled to view this course\'s material'
+		end
 	end
 end
